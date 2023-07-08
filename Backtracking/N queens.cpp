008025644685952e4a105e9b62eaf083e451d866
Ashutosh <ashutoshin2002@gmail.com>
2023-07-08 14:49:08 +0530

    newCommit

--- Backtracking/N queens.cpp
+++ Backtracking/N queens.cpp
@@ -3,16 +3,19 @@
 using namespace std;
 
 bool canPlaceQueen(int row,int col,vector<vector<char>> &grid){
+    //is queen attacking from verticle up
     for(int i = row -1;i>=0;i--){
         if(grid[i][col]=='Q'){
             return false;
         }
     }
+    //from diagonal left
     for(int i = row-1,j=col-1;i>=0 && j>=0;i--,j--){
         if(grid[i][j]=='Q'){
             return false;
         }
     }
+    //from diagonal right
     for(int i = row-1,j=col+1;i>=0 && j<grid.size();i--,j++){
         if(grid[i][j]=='Q'){
             return false;
@@ -22,6 +25,7 @@ bool canPlaceQueen(int row,int col,vector<vector<char>> &grid){
 }
 void nQueen(int currRow,int n,vector<vector<char>> &grid){
     //base condition
+    //is currRow = size of grid?
     if(currRow == n){
         for(int i =0;i<n;i++){
             for(int j = 0;j<n;j++){
@@ -31,11 +35,16 @@ void nQueen(int currRow,int n,vector<vector<char>> &grid){
         }
         cout<<"***"<<endl;
     }
-
+    
+    //iterate through each col in each row
     for(int col = 0;col<n;col++){
+        //chk weather we can place Q or not
         if(canPlaceQueen(currRow,col,grid)){
+            //if we can the place Q in that row,col
             grid[currRow][col] = 'Q';
+            //call recursive function for next row
             nQueen(currRow+1,n,grid);
+            //always revert the condition for next configration
             grid[currRow][col] = '.';
         }
     }
--- Backtracking/ratInMace.cpp
+++ Backtracking/ratInMace.cpp
@@ -1,53 +1,47 @@
-/* In 2D Matrix rat can go (upward,downward,forward,backward), find the number of ways that Rat can reach
-from upper left block to lower right block
-conditions: 
-rat can not move in 0 marked block it can only go through 1 marked block*/
 #include <iostream>
 #include <vector>
 using namespace std;
 
-bool CanWeGo(int i ,int j,vector<vector<int>> &grid){
-    return (i < grid.size() && j < grid.size() && i >= 0 && j >= 0) && grid[i][j] == 1;
+bool canWeGo(int i ,int j ,vector<vector<int>> & grid){
+    //chking if rat is allowed to go in next block or not
+    return (i < grid.size() && j < grid.size() && i >= 0 && j >= 0) && grid[i][j]==1;
 }
-int NoOfPaths(int i,int j,vector<vector<int>> &grid){
+int ratInMace(int i,int j, vector<vector<int>> & grid){
     int n = grid.size();
-    //Base case
-    if(i == n-1 && j == n-1){
-        // for(int i =0;i<n;i++){
-        //     for(int j =0;j<n;i++){
-        //         cout<<grid[i][j]<<" ";                
-        //     }cout<<endl;
-        // }
-        // cout<<"****";
-        return 1;
-    }
-
+    //base condition
+    if(i==(n-1) && j==(n-1)) return 1;
+    
+    //ans variable will store the no of path
     int ans = 0;
+    //mark so that rat can not travell same path again
     grid[i][j] = 2;
-    if(CanWeGo(i,j+1,grid)){//forward
-        ans += NoOfPaths(i,j+1,grid);
-    }
-    if(CanWeGo(i+1,j,grid)){//downward
-        ans += NoOfPaths(i+1,j,grid);
-    }
-    if(CanWeGo(i,j-1,grid)){//backward
-        ans += NoOfPaths(i,j-1,grid);
-    }
-    if(CanWeGo(i-1,j,grid)){//upward
-        ans += NoOfPaths(i-1,j,grid);
-    }
-    grid[i][j] == 1;
+    
+    if(canWeGo(i,j+1,grid)) //right
+        //keep adding ans once it found new path
+        ans+=ratInMace(i,j+1,grid);
+    
+    if(canWeGo(i+1,j,grid))  //down
+        ans+=ratInMace(i+1,j,grid);
+
+    if(canWeGo(i,j-1,grid))  // left
+        ans+=ratInMace(i,j-1,grid);
+
+    if(canWeGo(i-1,j,grid))  //up 
+        ans+=ratInMace(i-1,j,grid);
+    
+    //revert the mark so that new paths can be discovered
+    grid[i][j] = 1;
     return ans;
+    
 }
 int main(){
-    vector<vector<int>> grid = {
-        {1,1,1,1},
-        {0,1,0,1},
-        {0,1,1,1},
+    vector<vector<int>>grid{
+        {1,1,1,0},
+        {0,1,1,0},
+        {0,1,1,0},
         {0,1,1,1}
     };
-
-    int res = NoOfPaths(0,0,grid);
+    int res = ratInMace(0,0,grid);
     cout<<res<<"\n";
     return 0;
 }
